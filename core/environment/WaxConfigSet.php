7ad537b854ebbcd7c8a8c55d4f9ef0cfb41f01b4
hevelmo <hevelmo060683@gmail.com>
2017-06-02 14:48:35 -0500

    commit

--- core/environment/WaxConfigSet.php
+++ core/environment/WaxConfigSet.php
@@ -11,7 +11,7 @@
 
 $devServerList = array("127.0.0.1","::1","192.168.0.102","localhost");
 $url = $_SERVER['SERVER_NAME'];
-if ($url = "http://suzuki-lm.com.mx/") {
+if ($url = "suzuki-lm") {
     $folderDev = "suzuki-lm";    
     $location = "López Mateos";
 }
@@ -19,7 +19,7 @@ if ($url = "http://suzuki-lm.com.mx/") {
 if(!in_array($_SERVER['SERVER_NAME'], $devServerList)){
     $urlHost  = isset($_SERVER['HTTPS']) ? 'http://' : 'http://';
     $urlHost .= $_SERVER['SERVER_NAME'] . '/';
-    //$urlHost = str_replace ( "https" , "http" , $urlHost );
+    $urlHost = str_replace ( "https" , "http" , $urlHost );
     define("_HOST", $urlHost);
     define("_MAX", "http://clicktolead.com.mx/api/v1/remote/action");
 } else {
