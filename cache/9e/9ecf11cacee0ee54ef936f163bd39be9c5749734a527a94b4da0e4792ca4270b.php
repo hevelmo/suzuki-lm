9d18ca2cd2774eb7607bc68c0963091c6a6e0d7d
hevelmo <hevelmo060683@gmail.com>
2017-06-16 18:29:18 -0500

    commit

--- cache/9e/9ecf11cacee0ee54ef936f163bd39be9c5749734a527a94b4da0e4792ca4270b.php
+++ cache/9e/9ecf11cacee0ee54ef936f163bd39be9c5749734a527a94b4da0e4792ca4270b.php
@@ -19,12 +19,21 @@ class __TwigTemplate_37c6cb1de17187b9ea0d953d037c7dc106c3994b3e37df488c692f4dc6a
         echo "    <div class=\"specifications-wrapper\">
         <div class=\"specifications\" id=\"features-wrapper\">
             ";
-        // line 45
+        // line 30
         echo "            ";
-        // line 65
-        echo "            <!-- slide 00 -->
+        // line 41
+        echo "            <!-- slide extra -->
+            <div class=\"specification\" style=\"background-image: url('img/sections/home/slider/SLIDER-DE-PROMOCIONES-1659X655.jpg');\">
+                <div class=\"title-wrapper\" style=\"color: #fff\">
+                    <div class=\"title\">
+                       &nbsp;
+                    </div>
+                </div>
+                <div class=\"description compact\"><p>&nbsp;</p></div>
+            </div>
+            <!-- slide 00 -->
             <div class=\"specification\" style=\"background-image: url('";
-        // line 66
+        // line 51
         echo twig_escape_filter($this->env, (isset($context["_host"]) ? $context["_host"] : null), "html", null, true);
         echo "img/sections/home/slider/SLIDER-DE-PROMOCIONES-03.jpg');\">
                 <div class=\"title-wrapper\" style=\"color: #fff\">
@@ -35,10 +44,10 @@ class __TwigTemplate_37c6cb1de17187b9ea0d953d037c7dc106c3994b3e37df488c692f4dc6a
                 <div class=\"description compact\"><p>&nbsp;</p></div>
             </div>
             ";
-        // line 91
+        // line 76
         echo "            <!-- slide 02 -->
             <div class=\"specification\" style=\"background-image: url('";
-        // line 92
+        // line 77
         echo twig_escape_filter($this->env, (isset($context["_host"]) ? $context["_host"] : null), "html", null, true);
         echo "img/sections/home/slider/slider_marca-2017.jpg');\">
                 <div class=\"title-wrapper\" style=\"color: #fff\">
@@ -49,7 +58,7 @@ class __TwigTemplate_37c6cb1de17187b9ea0d953d037c7dc106c3994b3e37df488c692f4dc6a
                 <div class=\"description extra-compact\"><p>&nbsp;</p></div>
             </div>
             ";
-        // line 120
+        // line 105
         echo "        </div>
         <div class=\"clearfix\"></div>
     </div>
@@ -75,28 +84,13 @@ class __TwigTemplate_37c6cb1de17187b9ea0d953d037c7dc106c3994b3e37df488c692f4dc6a
 
     public function getDebugInfo()
     {
-        return array (  53 => 120,  42 => 92,  39 => 91,  28 => 66,  25 => 65,  23 => 45,  19 => 2,);
+        return array (  62 => 105,  51 => 77,  48 => 76,  37 => 51,  25 => 41,  23 => 30,  19 => 2,);
     }
 }
 /* {# INICIO DE NUEVO SLIDER #}*/
 /*     <div class="specifications-wrapper">*/
 /*         <div class="specifications" id="features-wrapper">*/
 /*             {#*/
-/*             <!-- slide extra -->*/
-/*             <div class="specification" style="background-image: url('img/sections/home/slider/SLIDER-DE-PROMOCIONES-swift.jpg');">*/
-/*                 <div class="title-wrapper" style="color: #fff">*/
-/*                     <div class="title">*/
-/*                        &nbsp;*/
-/*                     </div>*/
-/*                 </div>*/
-/*                 <div class="description extra-compact">*/
-/*                     <p>*/
-/*                         <a class='btn btn-sm btn-default' title='Nuevo Swift Boosterjet 2018' target='_blank' onclick="ga('send', 'event', 'Promoción Nuevo Swift', 'Home Slider', 'Nuevo Swift Boosterjet 2018');" href="http://suzukigdl.com.mx/nuevo-swift/">*/
-/*                             Ver más*/
-/*                         </a>*/
-/*                     </p>*/
-/*                 </div>*/
-/*             </div>*/
 /*             <!-- slide extra -->*/
 /*             <div class="specification" style="background-image: url('{{ _host }}img/sections/home/slider/SLIDER-DE-PROMOCIONES-1659X655-GUADALAJARA.jpg');">*/
 /*                 <div class="title-wrapper" style="color: #fff">*/
@@ -132,8 +126,9 @@ class __TwigTemplate_37c6cb1de17187b9ea0d953d037c7dc106c3994b3e37df488c692f4dc6a
 /*                 </div>*/
 /*                 <div class="description compact"><p>&nbsp;</p></div>*/
 /*             </div>*/
+/*             #}*/
 /*             <!-- slide extra -->*/
-/*             <div class="specification" style="background-image: url('img/sections/home/slider/SLIDER-DE-PROMOCIONES-05.jpg');">*/
+/*             <div class="specification" style="background-image: url('img/sections/home/slider/SLIDER-DE-PROMOCIONES-1659X655.jpg');">*/
 /*                 <div class="title-wrapper" style="color: #fff">*/
 /*                     <div class="title">*/
 /*                        &nbsp;*/
@@ -141,7 +136,6 @@ class __TwigTemplate_37c6cb1de17187b9ea0d953d037c7dc106c3994b3e37df488c692f4dc6a
 /*                 </div>*/
 /*                 <div class="description compact"><p>&nbsp;</p></div>*/
 /*             </div>*/
-/*             #}*/
 /*             <!-- slide 00 -->*/
 /*             <div class="specification" style="background-image: url('{{ _host }}img/sections/home/slider/SLIDER-DE-PROMOCIONES-03.jpg');">*/
 /*                 <div class="title-wrapper" style="color: #fff">*/
